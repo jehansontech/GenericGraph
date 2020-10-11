50bf57a248f55917ef240b7f518bce50bd423df0
Jim Hanson <jim@jehanson.org>
2020-10-11 15:40:01 -0400

    remove bad ideas

--- Sources/GenericGraph/Graph+Extensions.swift
+++ Sources/GenericGraph/Graph+Extensions.swift
@@ -23,34 +23,6 @@ extension Node: CustomStringConvertible {
     public var degree: Int {
         return inDegree + outDegree
     }
-
-    public func neighborhood(radius: Int) -> [NodeID: Node<N, E>] {
-        var nbhd = [NodeID: Node<N, E>]()
-    
-        if (radius >= 0) {
-            nbhd[self.id] = self
-        }
-        
-        if (radius >= 1) {
-            nbhd.merge(neighbors(), uniquingKeysWith: { x, y in return x })
-        }
-        
-        // TODO
-        
-        return nbhd
-    }
-    
-    public func neighbors() -> [NodeID: Node<N, E>] {
-        var nbrs = [NodeID: Node<N, E>]()
-        for edge in self.inEdges {
-            nbrs[edge.origin.id] = edge.origin
-        }
-        for edge in self.outEdges {
-            nbrs[edge.destination.id] = edge.destination
-        }
-        return nbrs
-    }
-    
 }
 
 extension Graph {
