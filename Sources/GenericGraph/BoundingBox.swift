94fadc9c775421ee474ac2b6c4a8e17f3fb60f1b
Jim Hanson <jim@jehanson.org>
2021-12-17 10:57:13 -0500

    graph actions stylin

--- Sources/GenericGraph/BoundingBox.swift
+++ Sources/GenericGraph/BoundingBox.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Wacoma
 
 public struct BoundingBox {
 
@@ -16,42 +17,6 @@ public struct BoundingBox {
     public var xMax: Float
     public var yMax: Float
     public var zMax: Float
-
-    public var xCenter: Float {
-        (xMin + xMax) / 2
-    }
-
-    public var yCenter: Float {
-        (yMin + yMax) / 2
-    }
-
-    public var zCenter: Float {
-        (zMin + zMax) / 2
-    }
-
-    public var min: SIMD3<Float> {
-        return SIMD3<Float>(xMin, yMin, zMin)
-    }
-    
-    public var max: SIMD3<Float> {
-        return SIMD3<Float>(xMax, yMax, zMax)
-    }
-
-    public var center: SIMD3<Float> {
-        return SIMD3<Float>(xCenter, yCenter, zCenter)
-    }
-
-    public var xBounds: ClosedRange<Float> {
-        return xMin...xMax
-    }
-
-    public var yBounds: ClosedRange<Float> {
-        return yMin...yMax
-    }
-
-    public var zBounds: ClosedRange<Float> {
-        return zMin...zMax
-    }
 
     public init(x0: Float, y0: Float, z0: Float, x1: Float, y1: Float, z1: Float) {
         xMin = Float.minimum(x0, x1)
@@ -72,7 +37,62 @@ public struct BoundingBox {
         yMax = point.y
         zMax = point.z
     }
-    
+}
+
+extension BoundingBox {
+
+    public var xCenter: Float {
+        xMin + xSize/2
+    }
+
+    public var yCenter: Float {
+        yMin + ySize/2
+    }
+
+    public var zCenter: Float {
+        zMin + zSize/2
+    }
+
+    public var xSize: Float {
+        xMax - xMin
+    }
+
+    public var ySize: Float {
+        yMax - yMin
+    }
+
+    public var zSize: Float {
+        zMax - zMin
+    }
+
+    public var min: SIMD3<Float> {
+        return SIMD3<Float>(xMin, yMin, zMin)
+    }
+
+    public var max: SIMD3<Float> {
+        return SIMD3<Float>(xMax, yMax, zMax)
+    }
+
+    public var center: SIMD3<Float> {
+        return SIMD3<Float>(xCenter, yCenter, zCenter)
+    }
+
+    public var size: SIMD3<Float>  {
+        return SIMD3<Float>(xSize, ySize, zSize)
+    }
+
+    public var xBounds: ClosedRange<Float> {
+        return xMin...xMax
+    }
+
+    public var yBounds: ClosedRange<Float> {
+        return yMin...yMax
+    }
+
+    public var zBounds: ClosedRange<Float> {
+        return zMin...zMax
+    }
+
     public mutating func cover(_ point: SIMD3<Float>) {
         if point.x < xMin {
             xMin = point.x
@@ -83,7 +103,7 @@ public struct BoundingBox {
         if point.z < zMin {
             zMin = point.z
         }
-        
+
         if point.x > xMax {
             xMax = point.x
         }
@@ -94,4 +114,5 @@ public struct BoundingBox {
             zMax = point.z
         }
     }
+
 }
